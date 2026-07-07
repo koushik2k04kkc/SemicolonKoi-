3bf8701fab19e8a720886f3254f8434734e65890
baruni8march <golumolu30june@gmail.com>
2026-07-07 20:46:00 +0600

    Changed folder structure

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const app = express();
-jcjcknkdvkfnfk
-fbnkbnfkbkbkbkfnbkfnbkfnbknbkfnbknbkfnbkfnbknfbknbknfbkfnbkfnb
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-}
